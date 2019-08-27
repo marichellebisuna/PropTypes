41fe36655767aefd1a0baa8471c688fa1f070faf
MARICHELLE BISUNA <marichellebisuna@yahoo.com>
2019-08-27 11:59:41 +1000

    PropTypes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import FeaturedProduct from './HomePage/FeaturedProduct';
+
+function App() {
+	return (
+		<div className="App">
+			
+			<FeaturedProduct />
+		</div>
+	);
+}
+
+export default App;
